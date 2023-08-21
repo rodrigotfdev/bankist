b48969731a53c8f85e0fe7edfa8b20f8c629f48b
rodrigotfcode <52326702+rodrigotfcode@users.noreply.github.com>
2023-08-21 17:07:14 -0300

    Display incomes, outcomes and interest rate function

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ const displayMovments = function (movements) {
         ${index + 1} ${type}
         </div>
       <div class="movements__value">
-        ${movement}
+        ${movement} €
       </div>
     </div>
     `;
@@ -83,9 +83,33 @@ displayMovments(account1.movements);
 
 const calcDisplayBalance = function (movements) {
   const balance = movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${balance} EUR`;
+  labelBalance.textContent = `${balance} €`;
 };
-calcDisplayBalance(account1.movements)
+
+calcDisplayBalance(account1.movements);
+
+const calcDisplaySummary = function (movements) {
+  const incomes = movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes} €`;
+
+  const outcomes = movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${outcomes * -1} €`;
+
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * 1.2) / 100)
+    .filter((int, i, arr) => {
+      console.log(arr)
+      return int >= 1
+    })
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest} €`;
+};
+calcDisplaySummary(account1.movements);
 
 const createUsernames = function (accs) {
   accs.forEach(function (acc) {
@@ -98,7 +122,6 @@ const createUsernames = function (accs) {
 };
 
 createUsernames(accounts);
-
 
 console.log(createUsernames('Steven Thomas Williams'));
 
